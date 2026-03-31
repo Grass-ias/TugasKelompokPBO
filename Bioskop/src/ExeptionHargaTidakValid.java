53f1198481bb76e519675f0d7e04fc3c4d40e5ed
Grass-ias <ferdyprasetyaputra08@gmail.com>
2026-03-31 23:22:46 +0700

    nambah nama

--- Bioskop/src/ExeptionHargaTidakValid.java
+++ Bioskop/src/ExeptionHargaTidakValid.java
@@ -1,3 +1,10 @@
+/* Nama Anggota Kelompok:
+Crystiano Bayu Satya Alves 24060124140130
+Caesar Ferdiana Suwandi 24060124120051
+Ferdy Prasetya Putra 24060124140145
+Ghatfan Muhammad Atiwiar 24060124140171
+*/
+
 public class ExeptionHargaTidakValid extends IllegalArgumentException {
     public ExeptionHargaTidakValid(String message) {
         super(message);
--- Bioskop/src/Pelanggan.java
+++ Bioskop/src/Pelanggan.java
@@ -1,3 +1,10 @@
+/* Nama Anggota Kelompok:
+Crystiano Bayu Satya Alves 24060124140130
+Caesar Ferdiana Suwandi 24060124120051
+Ferdy Prasetya Putra 24060124140145
+Ghatfan Muhammad Atiwiar 24060124140171
+*/
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
